c5591356dbe57a8e6caeacf04ab13c1fe0837c4d
Munazza.Shaikh <munazzashaikh531@gmail.com>
2026-05-18 12:07:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,7 @@
    CONFIG
 ───────────────────────────────────────────── */
 const CONFIG = {
-  REMOVE_BG_API: 'KPZW2fFUwFJ6SMgD6q21uuEt',
+  REMOVE_BG_API: 'a9sd1C2xXSQvtwG4Lrjqxn7G',
   ACTIVATION_CODE: '@Munazza#',
   DEV_EMAIL: 'munazzashaikh531@gmail.com',
   PLANS: {
